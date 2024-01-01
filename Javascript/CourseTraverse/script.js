964982d1fe30401cd0d7cd048a7ebe4c7758d91f
Derek <dduong03@uoguelph.ca>
2024-01-01 16:09:26 -0500

    update

---
+++ Javascript/CourseTraverse/script.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+const count = 0;
+
+console.error("This is an error message");
+
+console.warn("This is a warning");
+
+console.table({a: 1, b: 2});
+
+const styles = 'color: blue; font-size: 20px; background: yellow; padding: 10px';
+
+console.log('%cHello World!', styles);
---
+++ Javascript/variables/variables.js
@@ -0,0 +1,7 @@
+//ways to declare a variable
+// var, let, const
+
+let firstName = "John";
+let lastName = "Doe";
+var age = 35;
+console.log(firstName, lastName, age);

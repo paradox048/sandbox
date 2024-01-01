a9edcc1cba4ecc9c9296c6a7a43f04448b9940f6
Derek <dduong03@uoguelph.ca>
2024-01-01 15:27:09 -0500

    starting commit

---
+++ Javascript/variables.js
@@ -0,0 +1,7 @@
+//Declaring and initializing variables in JS
+//To run the code, type 'node variables.js' (without '') in the terminal
+
+let count = 0;
+let first_num = 1;
+count += first_num;
+console.log(count);
